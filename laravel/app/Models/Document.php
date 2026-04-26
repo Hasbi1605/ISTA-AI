d4dcdbb7f307a48616fb08e9987938fbd9181a62
Hasbi1605 <terry.delvon0805@gmail.com>
2026-04-26 07:24:52 +0700

    Revert "[AI Parity] Embedding Cascade GitHub Models dan Storage Vector Laravel (#97)"
    This reverts commit 5f02acd4be5d1f5f8e8270716825b582548fda30.

--- laravel/app/Models/Document.php
+++ laravel/app/Models/Document.php
@@ -60,9 +60,4 @@ class Document extends Model
     {
         return $this->belongsTo(User::class);
     }
-
-    public function chunks(): \Illuminate\Database\Eloquent\Relations\HasMany
-    {
-        return $this->hasMany(DocumentChunk::class);
-    }
 }
